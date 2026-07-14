9deb4e80c39028ba9a55063eed07e886c956d986
abdullahbasir <abdullah.sbasir@gmail.com>
2026-07-14 10:57:09 +0400

    Downloaded the rabbit message broker from docker and created a connection in the cmd/server folder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connection := "amqp://guest:guest@localhost:5672/"
+
+	newCon, err := amqp.Dial(connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer newCon.Close()
+
+	fmt.Println("Connection is Successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Program is shutting down")
 }
