7c6e30b4ac0e92b77d8a0094da3d1658e96c3193
abdullahbasir <abdullah.sbasir@gmail.com>
2026-07-15 20:14:20 +0400

    Grouped the handlers into one file

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 
 	"github.com/AbdullahBasir/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/AbdullahBasir/learn-pub-sub-starter/internal/routing"
 )
+
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
+	return func(ps routing.PlayingState) {
+		defer fmt.Print("> ")
+		gs.HandlePause(ps)
+	}
+}
 
 func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
 	return func(move gamelogic.ArmyMove) {
